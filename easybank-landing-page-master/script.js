f0c67adc58f5edde36a52edc5cbf3f6575a04242
Mohd Saquib <ans2sqb@gmail.com>
2020-03-20 16:33:41 +0530

    active state fixed

--- easybank-landing-page-master/script.js
+++ easybank-landing-page-master/script.js
@@ -1,13 +1,32 @@
 const hamburger = document.querySelector(".hamburger");
 const navbar = document.querySelector('.navbar');
 const hero = document.querySelector('.hero');
-
+const bottomHover = document.querySelector('.bottom-hover');
+const navItems = document.querySelectorAll(".menu li");
 
 function toggleMenu() {
     hamburger.classList.toggle("active");
     navbar.classList.toggle('active');
     hero.classList.toggle('overlay');
 }
+
+function showBottomHover() {
+    const coords = this.getBoundingClientRect();
+
+    bottomHover.classList.add('show');
+
+    const top = coords.top + 30;
+    const newCoords = {
+        top: `${top}px`,
+        left: `${coords.left}px`,
+        width: `${coords.width}px`,
+    }
+    bottomHover.style.top = newCoords.top;
+    bottomHover.style.left = newCoords.left;
+    bottomHover.style.width = newCoords.width;
+
+}
+
 
 hamburger.addEventListener('click', toggleMenu);
 hero.addEventListener('click', () => {
@@ -19,4 +38,6 @@ window.addEventListener('scroll', () => {
     hamburger.classList.remove("active");
     navbar.classList.remove('active');
     hero.classList.remove('overlay');
-})
+});
+
+navItems.forEach(navItem => navItem.addEventListener('mouseenter', showBottomHover));
